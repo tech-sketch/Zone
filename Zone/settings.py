227dd58b9db597ab0d54d0f34fb7a494e7c26fa5
unknown <tie304108@S1504C10013-T1.tisnt.tis.co.jp>
2015-06-09 16:59:09 +0900

    create CSV_to_DB.py

--- Zone/settings.py
+++ Zone/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'Zone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Zone',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'NAME': 'xxxxxxxx',
+        'USER': 'xxxxxxxx',
+        'PASSWORD': 'xxxxxxxx',
     }
 }
 
