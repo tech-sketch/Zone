3f61e9ca913449d7a9c2536616f6acf4819354fb
sakamoto ryota <saka_ro@yahoo.co.jp>
2015-06-16 13:43:27 +0900

    Add the map.css

--- Zone/settings.py
+++ Zone/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'Zone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'xxxxxxx',
-        'USER': 'xxxxxxx',
-        'PASSWORD': 'xxxxxxx',
+        'NAME': 'Zone',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
     }
 }
 
