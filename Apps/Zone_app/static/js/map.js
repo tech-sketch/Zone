1f359d62a18e2d106433097d7508e85d36b43bc1
unknown <tie304108@S1504C10013-T1.tisnt.tis.co.jp>
2015-06-16 15:11:57 +0900

    Add function that displays the makers of all places on map.html

--- Apps/Zone_app/static/js/map.js
+++ Apps/Zone_app/static/js/map.js
@@ -24,15 +24,53 @@ function initialize(x, y) {
         zoom: 17
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var markerImg = new google.maps.MarkerImage(
+                // マーカーの画像URL
+                "http://127.0.0.1:8000/static/images/man.png",
+
+                // マーカーのサイズ
+                new google.maps.Size(32, 32),
+
+                // 画像の基準位置
+                new google.maps.Point(0, 0),
+
+                // Anchorポイント
+                new google.maps.Point(10, 24)
+            );
+
+
     var userMarker = new google.maps.Marker({
             position: myLatlng,
             map: map,
-            title:"Your position"
+            title:"Your position",
+            icon: markerImg,
     });
     new google.maps.InfoWindow({
                 content: "現在地"
               }).open(map, userMarker)
+    makePlacePin();
 }
+
+function makePlacePin() {
+    for(var i = 0; i < $("[id = name]").length; i++){
+        var placeLatlng = new google.maps.LatLng( $($("[id = latitude]")[i]).attr("value"), $($("[id=longitude]")[i]).attr("value"));
+        new google.maps.Marker({
+            position: placeLatlng,
+            map: map,
+            title: $($("[id = name]")[i]).attr("value"),
+    });
+    }
+    $("[id = name]").each(function(index,data){
+        console.log($(data).attr("value"));
+    });
+    $("[id = longitude]").each(function(index,data){
+        console.log($(data).attr("value"));
+    });
+    $("[id = latitude]").each(function(index,data){
+        console.log($(data).attr("value"));
+    });
+}
+
 function codeAddress() {
     var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address, region: 'JP'}, function(results, status) {
