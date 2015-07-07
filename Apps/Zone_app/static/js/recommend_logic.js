ac2e458ff04fb18ff369381d31a3ad521d164935
unknown <tie304108@S1504C10013-T1.tisnt.tis.co.jp>
2015-07-07 11:17:35 +0900

    Add else statement for an if statement in views.search

--- Apps/Zone_app/static/js/recommend_logic.js
+++ Apps/Zone_app/static/js/recommend_logic.js
@@ -17,28 +17,21 @@ function showRecommendForm(html, placeId){
 }
 
 function detailRecommend(){
-    console.log("detailRecommend")
-    console.log($("#user_auth").attr("value"))
     if($("#user_auth").attr("value") == "False"){
         location.href = "/new"
     }
     $.get("/recommend_form/", function(html){
         showRecommendForm(html, $("#detail_place_id").attr("value"))
     })
-
 }
 
 function saveRecommend() {
     var point = $("#point").val();
-    console.log(point);
     moods = [];
     var answer = $("input[name='moods']:checked").each(function(index, element){
         moods.push($(element).val());
     });
     var place = $("#recommend").attr("value");
-
-    console.log(recommendPlaceId)
-    //console.log("place_id:" + recommendPlaceId)
     $.post("/save_recommend/",{point: point, moods: moods, place: recommendPlaceId}, function(data){
         $("#user_point").text("現在のpoint:" + data.split(",")[1])
         bootbox.alert(data.split(',')[0])
--- Apps/Zone_app/views.py
+++ Apps/Zone_app/views.py
@@ -66,7 +66,7 @@ def search(request):
     address = request.POST['address']
     place_name = request.POST['place_name']
     result = connect_geocode_api(address)
-    if result['status']=='OK':
+    if result['status'] == 'OK':
         location = result['results'][0]['geometry']['location']
         northeast = result['results'][0]['geometry']['viewport']['northeast']
         southwest = result['results'][0]['geometry']['viewport']['southwest']
@@ -76,7 +76,8 @@ def search(request):
                                      longitude__lt=location['lng']+rate*LNG_FROM_CEN,
                                      latitude__gt=location['lat']-rate*LAT_FROM_CEN,
                                      latitude__lt=location['lat']+rate*LAT_FROM_CEN)
-    all_place = all_place.filter(name__icontains=place_name)
+    else:
+        pass
     places = sort_by_point(all_place)
     moods = Mood.objects.all()
     return render_to_response('map.html', {'places': places, 'moods': moods, 'address': address,
