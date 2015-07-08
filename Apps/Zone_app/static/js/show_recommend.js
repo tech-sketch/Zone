c5d3f3b4ad3f235970c984ee72950628beacf726
unknown <tie304108@S1504C10013-T1.tisnt.tis.co.jp>
2015-07-08 11:36:31 +0900

    Modify layout of detail dialog

--- Apps/Zone_app/static/js/show_recommend.js
+++ Apps/Zone_app/static/js/show_recommend.js
@@ -3,18 +3,6 @@ function showRecommend(html){
         title: "あなたにお勧めの場所があります",
         message: html,
         size: "large",
-        buttons: {
-            checkIn: {
-                label: "チェックイン（10ポイントゲット）",
-                className: "btn-primary",
-                callback: detailCheckIn
-            },
-            recommend: {
-                label: "この場所をおすすめする",
-                className: "btn-primary",
-                callback: detailRecommend
-            },
-        }
     });
 }
 function startRecommend(){
