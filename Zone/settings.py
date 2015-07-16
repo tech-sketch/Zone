9c9f9f1c8851763792ac8edac2e6051cb95a8a45
Yuki-Inamoto <inamoto.yuki@tis.co.jp>
2015-07-16 14:50:34 +0900

    fix comment

--- Zone/settings.py
+++ Zone/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Zone',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': '17suraimuAAA',
     }
 }
 
